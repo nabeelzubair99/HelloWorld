1ea2e2cfb745a764fe357d53fe41718a77b19d6c
nabeelzubair99 <nabeelzubair99@yahoo.com>
2020-08-27 02:26:02 -0400

    my first java program

---
+++ Program Design/src/main/java/ProgramDesign/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProgramDesign;
+
+/**
+ *
+ * @author nabee
+ */
+import java.time.LocalDateTime;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Nabeel Zubair
+        //8-27-2020, Assignment 1 Greetings, 2:15 am
+        //this program is my first program to print out a statement
+        LocalDateTime myCurrentTime = LocalDateTime.now();
+        System.out.println("Good Evening Professor it is"+ " "+ myCurrentTime);
+    }
+    
+}
